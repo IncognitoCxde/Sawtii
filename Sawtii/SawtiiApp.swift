e3f1063ac065f0ad38a340757ea78cab2b81e067
IncognitoCxde <benissaeaston@gmail.com>
2026-02-13 11:02:00 +0500

    Initial Commit

---
+++ Sawtii/SawtiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SawtiiApp.swift
+//  Sawtii
+//
+//  Created by iMacbook on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SawtiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
